0e0fc6a9047ec728c90ec585d29596a1341ba019
Scott Fisk <scottfisk@me.com>
2025-05-03 09:17:39 +1000

    Update workshop.blade.php

--- resources/views/filament/server/pages/workshop.blade.php
+++ resources/views/filament/server/pages/workshop.blade.php
@@ -92,7 +92,7 @@
                         <h3 class="text-lg font-medium text-gray-900 dark:text-white">Available Mods</h3>
                         <div class="flex space-x-2 items-center">
                             <div class="w-60">
-                                <div class="fi-input-wrp flex h-10.5 rounded-lg shadow-sm ring-1 transition duration-75 bg-white dark:bg-white/5 focus-within:ring-2 ring-gray-950/10 dark:ring-white/20 focus-within:ring-primary-600 dark:focus-within:ring-primary-500">
+                                <div class="fi-input-wrp flex h-10 rounded-md shadow-sm ring-1 transition duration-75 bg-white dark:bg-gray-800 dark:ring-white/20 focus-within:ring-2 focus-within:ring-primary-500 ring-gray-300 dark:border-gray-700">
                                     <div class="fi-input-wrp-prefix items-center gap-x-3 ps-3 flex pe-2">
                                         <svg wire:loading.remove.delay.default="1" wire:target="searchTerm" class="fi-input-wrp-icon h-5 w-5 text-gray-400 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clip-rule="evenodd"></path>
@@ -106,7 +106,7 @@
                                     
                                     <div class="fi-input-wrp-input min-w-0 flex-1">
                                         <input 
-                                            class="fi-input block w-full h-full border-none text-base text-gray-950 transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6 bg-white/0 ps-0 pe-3" 
+                                            class="fi-input block w-full h-10 border-0 py-1.5 text-gray-900 dark:text-white shadow-none ring-0 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 bg-transparent"
                                             autocomplete="off" 
                                             placeholder="Search mods..." 
                                             type="search" 
@@ -118,7 +118,7 @@
                             
                             <select 
                                 wire:model.live="currentSort"
-                                class="bg-white dark:bg-gray-800 text-gray-900 dark:text-white border border-gray-300 dark:border-gray-700 rounded-md shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"
+                                class="h-10 bg-white dark:bg-gray-800 text-gray-900 dark:text-white border border-gray-300 dark:border-gray-700 rounded-md shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50 px-3 py-2"
                             >
                                 <option value="popular">Popular</option>
                                 <option value="newest">Newest</option>
