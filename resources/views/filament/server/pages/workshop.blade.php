64a5acb3f237813a6b9221e8c459e9365e00b836
Scott Fisk <scottfisk@me.com>
2025-05-03 09:18:52 +1000

    Update workshop.blade.php

--- resources/views/filament/server/pages/workshop.blade.php
+++ resources/views/filament/server/pages/workshop.blade.php
@@ -92,21 +92,21 @@
                         <h3 class="text-lg font-medium text-gray-900 dark:text-white">Available Mods</h3>
                         <div class="flex space-x-2 items-center">
                             <div class="w-60">
-                                <div class="fi-input-wrp flex h-10 rounded-md shadow-sm ring-1 transition duration-75 bg-white dark:bg-gray-800 dark:ring-white/20 focus-within:ring-2 focus-within:ring-primary-500 ring-gray-300 dark:border-gray-700">
-                                    <div class="fi-input-wrp-prefix items-center gap-x-3 ps-3 flex pe-2">
-                                        <svg wire:loading.remove.delay.default="1" wire:target="searchTerm" class="fi-input-wrp-icon h-5 w-5 text-gray-400 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <div class="fi-input-wrp flex h-10 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 focus-within:ring focus-within:ring-primary-500 focus-within:ring-opacity-50 focus-within:border-primary-500">
+                                    <div class="fi-input-wrp-prefix items-center flex ps-3 pe-2">
+                                        <svg wire:loading.remove.delay.default="1" wire:target="searchTerm" class="h-5 w-5 text-gray-400 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clip-rule="evenodd"></path>
                                         </svg>
                                         
-                                        <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="animate-spin fi-input-wrp-icon h-5 w-5 text-gray-400 dark:text-gray-500" wire:loading.delay.default="" wire:target="searchTerm">
+                                        <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="animate-spin h-5 w-5 text-gray-400 dark:text-gray-500" wire:loading.delay.default="" wire:target="searchTerm">
                                             <path clip-rule="evenodd" d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill-rule="evenodd" fill="currentColor" opacity="0.2"></path>
                                             <path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="currentColor"></path>
                                         </svg>
                                     </div>
                                     
-                                    <div class="fi-input-wrp-input min-w-0 flex-1">
+                                    <div class="flex-1">
                                         <input 
-                                            class="fi-input block w-full h-10 border-0 py-1.5 text-gray-900 dark:text-white shadow-none ring-0 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 bg-transparent"
+                                            class="w-full h-10 border-0 py-0 px-0 text-gray-900 dark:text-white shadow-none ring-0 focus:ring-0 focus:outline-none bg-transparent"
                                             autocomplete="off" 
                                             placeholder="Search mods..." 
                                             type="search" 
